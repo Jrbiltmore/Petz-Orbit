4d306d49827801790d65b0514316cebf7139019d
Jacob Thomas Redmond <127049997+Jrbiltmore@users.noreply.github.com>
2023-07-18 23:12:21 -0700

    Update DeletedUserRepository.java
    Expanded version of the DeletedUserRepository interface to incorporate additional methods for the DeletedUser entity. These methods enable searching for deleted users based on various criteria, including username, email, role, deletion status, date/time range, partial matching, and searching by a list of roles.

--- find-my-pet/src/main/java/ru/nsu/sberlab/repositories/DeletedUserRepository.java
+++ find-my-pet/src/main/java/ru/nsu/sberlab/repositories/DeletedUserRepository.java
@@ -4,6 +4,45 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.nsu.sberlab.models.entities.DeletedUser;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface DeletedUserRepository extends JpaRepository<DeletedUser, Long> {
+    Optional<DeletedUser> findByUsername(String username);
+    
+    List<DeletedUser> findByEmail(String email);
+    
+    List<DeletedUser> findByRole(String role);
+    
+    List<DeletedUser> findByDeletedAtIsNull();
+    
+    List<DeletedUser> findByDeletedAtIsNotNull();
+    
+    List<DeletedUser> findByDeletedAtBetween(LocalDateTime startDateTime, LocalDateTime endDateTime);
+    
+    List<DeletedUser> findByDeletedAtBefore(LocalDateTime dateTime);
+    
+    List<DeletedUser> findByDeletedAtAfter(LocalDateTime dateTime);
+    
+    void deleteByUsername(String username);
+    
+    void deleteByEmail(String email);
+    
+    void deleteByRole(String role);
+    
+    void deleteByDeletedAtBefore(LocalDateTime dateTime);
+    
+    void deleteByDeletedAtAfter(LocalDateTime dateTime);
+    
+    List<DeletedUser> findByUsernameContainingIgnoreCase(String keyword);
+    
+    List<DeletedUser> findByEmailContainingIgnoreCase(String keyword);
+    
+    List<DeletedUser> findByRoleContainingIgnoreCase(String keyword);
+    
+    List<DeletedUser> findByUsernameOrEmailContainingIgnoreCase(String usernameKeyword, String emailKeyword);
+    
+    List<DeletedUser> findByRoleIn(List<String> roles);
 }
