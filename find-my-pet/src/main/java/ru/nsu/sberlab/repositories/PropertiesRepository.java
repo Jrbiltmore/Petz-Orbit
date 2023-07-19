64a2a94752311372f815df784b9b6fb51bf3dcba
Jacob Thomas Redmond <127049997+Jrbiltmore@users.noreply.github.com>
2023-07-18 23:16:40 -0700

    Update PropertiesRepository.java
    This commit adds a touch of magic to the PropertiesRepository interface! We've expanded it with some amazing methods to make querying and filtering property types a breeze. With these additions, you can now easily find property types by their name, enabled status, category, and even perform partial matching searches. It's like having a wizard at your fingertips, granting you the power to effortlessly retrieve the exact property types you desire. So go ahead and dive into the enchanting world of property management with this upgraded PropertiesRepository interface! May your coding journey be filled with joy and whimsy! ✨🔮✨

--- find-my-pet/src/main/java/ru/nsu/sberlab/repositories/PropertiesRepository.java
+++ find-my-pet/src/main/java/ru/nsu/sberlab/repositories/PropertiesRepository.java
@@ -3,5 +3,35 @@ package ru.nsu.sberlab.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.nsu.sberlab.models.entities.PropertyType;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PropertiesRepository extends JpaRepository<PropertyType, Long> {
+    Optional<PropertyType> findByPropertyName(String propertyName);
+    
+    List<PropertyType> findByEnabled(boolean enabled);
+    
+    List<PropertyType> findByCategory(String category);
+    
+    List<PropertyType> findByCategoryAndEnabled(String category, boolean enabled);
+    
+    List<PropertyType> findByPropertyNameContainingIgnoreCase(String keyword);
+    
+    List<PropertyType> findByDescriptionContainingIgnoreCase(String keyword);
+    
+    List<PropertyType> findByCategoryContainingIgnoreCase(String keyword);
+    
+    List<PropertyType> findByEnabledAndCategoryContainingIgnoreCase(boolean enabled, String keyword);
+    
+    List<PropertyType> findByPropertyNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String propertyKeyword, String descriptionKeyword);
+    
+    List<PropertyType> findByEnabledAndCategoryIn(boolean enabled, List<String> categories);
+    
+    List<PropertyType> findByCategoryIn(List<String> categories);
+    
+    List<PropertyType> findByEnabledAndCategoryAndPropertyNameContainingIgnoreCase(boolean enabled, String category, String keyword);
+    
+    List<PropertyType> findByEnabledAndCategoryAndDescriptionContainingIgnoreCase(boolean enabled, String category, String keyword);
+    
+    List<PropertyType> findByEnabledAndCategoryAndPropertyNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(boolean enabled, String category, String propertyKeyword, String descriptionKeyword);
 }
